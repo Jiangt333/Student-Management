f5c9298b6dbda54f4b8ec4c86c9cf5bb47595aaf
weigp1 <2524634086@qq.com>
2024-03-12 15:35:43 +0800

    简单登录页

--- front/src/main.js
+++ front/src/main.js
@@ -1,11 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import ElementPlus from 'element-plus'// 导入ElementPlus
+import 'element-plus/dist/index.css'
+// 导入css文件
+import "./style.css";
+import './assets/base.css'
+import './assets/iconfont/iconfont.css'
 
-import './assets/main.css'
+import { createPinia } from "pinia"; // 引入pinia
+import { router } from "./routes/router"; // 引入路由
+import { UserStore } from "./stores/UserStore" // 引入UserStore
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(ElementPlus) // 使用Element UI
 
-app.mount('#app')
+app.use(createPinia()); // 引入pinia
+const userStore = UserStore()
+
+app.use(router); // 引入路由
+
+app.mount("#app");
---
+++ front/src/views/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <meta charset="charset=utf-8"/>
+    <div class="bgimg">
+      <div>
+        <el-card class="card">
+          <el-button class="login" onclick="window.location.href = 'https://gitlab.igem.org/2023/software-tools/sysu-software'">
+          登录
+          </el-button>
+          <el-button class="help">
+          忘记密码
+          </el-button>
+          <el-button class="help">
+          需要帮助
+          </el-button>
+        </el-card>
+      </div>
+    </div>
+
+</template>
+  
+<script  setup>
+import {ref} from 'vue'
+import {useRoute, useRouter} from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+</script>
+
+<style scoped>
+.bgimg{
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  background-image: url('../assets/body.jpg');
+  background-size:100% 100%;
+}
+
+.login{
+  z-index: 1;
+  margin-top:5%;
+  margin-left: 10%;
+  margin-right: 10%;
+  border-radius: 2rem;
+  height: 3rem;
+  width: 80%;
+  font-size: 1.2rem;
+  border: none;
+  background-color: rgba(12, 64, 196, 0.58);
+  color: aliceblue;
+}
+.help{
+  z-index: 1;
+  margin-top:7%;
+  margin-left: 5%;
+  margin-right: 5%;
+  border-radius: 2rem;
+  height: 50%;
+  width: 100%;
+  font-size: 1rem;
+  width: 40%;
+  border: none;
+}
+.card{
+  margin-top:15%;
+  margin-left: 60%;
+  opacity: 80%;
+  width: 23%;
+  height: 11rem;
+  color: aliceblue;
+}
+</style>
+  
