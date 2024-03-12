ceadff739ff3b3d14df6a5eec1e75bb51db67698
jiangt <jt1170504445@163.com>
2024-03-12 20:12:09 +0800

    feat: 重构后端，改为微服务架构，初步配置了swagger

---
+++ front/src/routes/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+let routes = [
+    { path: "/", component: () => import("../views/Login.vue"), },
+    { path: "/home", component: () => import("../views/Home.vue"), },
+    
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export { router, routes }
